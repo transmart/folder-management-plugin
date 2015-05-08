5e215f387668709cb002009e8671306b85b9f5f3
Terry-Weymouth <weymouth@umich.edu>
2015-05-08 15:46:13 -0400

    another attempt

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,10 +44,6 @@ grails.project.dependency.resolution = {
         compile(':spring-security-core:2.0-RC2')
         //compile ':spring-security-ldap:2.0-RC2')
         compile(':quartz:1.0-RC2')
-        build(":release:3.1.1",
-                ":rest-client-builder:1.0.3") {
-            export = false
-        }
-
+        build(":release:3.1.1")
     }
 }
